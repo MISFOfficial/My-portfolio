d66729e534a5540491cd27d5948dc200f08a3e4a
Muksitul Islam <muksitul15-3962@diu.edu.bd>
2025-07-03 00:51:52 +0600

    modify

--- src/Component/Navigation/FooterNav.jsx
+++ src/Component/Navigation/FooterNav.jsx
@@ -8,23 +8,23 @@ import { GrProjects, GrResume } from 'react-icons/gr';
 const FooterNav = () => {
     return (
         <div className='flex items-center gap-2'>
-            <NavLink to="/" className={({ isActive }) => isActive ? 'text-green-500' : 'text-white'}>
-                <li className='border p-2 border-gray-400 rounded-md flex flex-col items-center justify-center gap-1 bg-gradient-to-r from-[#1b1b1b] to-[#464a4a] duration-200'>
+            <NavLink to="/" className={({ isActive }) => isActive ? 'text-green-500 border border-green-600 rounded-md' : 'text-white border border-gray-400 rounded-md'}>
+                <li className=' p-2  rounded-md flex flex-col items-center justify-center gap-1 bg-gradient-to-r from-[#1b1b1b] to-[#464a4a] duration-200'>
                     <div className=''><TbSmartHome></TbSmartHome></div>
                 </li>
             </NavLink>
-            <NavLink to="/resume" className={({ isActive }) => isActive ? 'text-green-500' : 'text-white'}>
-                <li className='border p-2 border-gray-400 rounded-md flex flex-col items-center justify-center gap-1 bg-gradient-to-r from-[#1b1b1b] to-[#464a4a] duration-200'>
+            <NavLink to="/resume" className={({ isActive }) => isActive ? 'text-green-500 border border-green-600 rounded-md' : 'text-white border border-gray-400 rounded-md'}>
+                <li className=' p-2  rounded-md flex flex-col items-center justify-center gap-1 bg-gradient-to-r from-[#1b1b1b] to-[#464a4a] duration-200'>
                     <div className=''><GrResume></GrResume></div>
                 </li>
             </NavLink>
-            <NavLink to="/projects" className={({ isActive }) => isActive ? 'text-green-500' : 'text-white'}>
-                <li className='border p-2 border-gray-400 rounded-md flex flex-col items-center justify-center gap-1 bg-gradient-to-r from-[#1b1b1b] to-[#464a4a] duration-200'>
+            <NavLink to="/projects" className={({ isActive }) => isActive ? 'text-green-500 border border-green-600 rounded-md' : 'text-white border border-gray-400 rounded-md'}>
+                <li className=' p-2  rounded-md flex flex-col items-center justify-center gap-1 bg-gradient-to-r from-[#1b1b1b] to-[#464a4a] duration-200'>
                     <div className=''><GrProjects></GrProjects></div>
                 </li>
             </NavLink>
-            <NavLink to="/contact" className={({ isActive }) => isActive ? 'text-green-500' : 'text-white'}>
-                <li className='border p-2 border-gray-400 rounded-md flex flex-col items-center justify-center gap-1 bg-gradient-to-r from-[#1b1b1b] to-[#464a4a] duration-200'>
+            <NavLink to="/contact" className={({ isActive }) => isActive ? 'text-green-500 border border-green-600 rounded-md' : 'text-white border border-gray-400 rounded-md'}>
+                <li className=' p-2  rounded-md flex flex-col items-center justify-center gap-1 bg-gradient-to-r from-[#1b1b1b] to-[#464a4a] duration-200'>
                     <div className=''><FaTelegramPlane></FaTelegramPlane></div>
                 </li>
             </NavLink>
